1204f450fd8e99bca3c381919f4adbca4ce9ea0d
Vitor Emidio <vitorsemidio.96@gmail.com>
2023-02-21 02:45:01 +0000

    chore: criação bash script
    script de criação de estrutura de usuários, diretórios e permissões

---
+++ README.md
@@ -0,0 +1,38 @@
+# Infraestrutura como Código: Script de Criação de Estrutura de Usuários, Diretórios e Permissões
+
+## Diretórios
+
+- /publico
+- /adm
+- /ven
+- /sec
+
+## Grupos
+
+- GRP_ADM
+- GRP_VEN
+- GRP_SEC
+
+## Usuários
+
+- carlos
+- maria
+- joao
+- debora
+- sebastiana
+- roberto
+- josefina
+- amanda
+- rogerio
+
+## Definições
+
+- Excluir diretórios, arquivos, grupos e usuários criados anteriormente;
+- Todo provisionamento deve ser feito em um arquivo do tipo Bash Script;
+- O dono de todos os diretórios criados será o usuário root;
+- Todos os usuários terão permissão dentro do diretório **publico**;
+- Os usuários de cada grupo terão permissão total dentro de seu respectivo diretório;
+- Os usuaŕios não poderão ter permissão de leitura, escrita e execução em diretórios de departamentos que eles não pertencem;
+- Subir arquivo de script criado para a sua conta no Github.
+
+
---
+++ iac.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# Criacao diretorios
+
+mkdir /publico
+mkdir /adm
+mkdir /ven
+mkdir /sec
+
+
+# Criacao grupos
+groupadd GRP_ADM
+groupadd GRP_VEN
+groupadd GRP_SEC
+
+
+# Criacao usuarios
+## Usuarios grupo GRP_ADM
+useradd carlos -c "Carlos" -m -G GRP_ADM -p $(openssl passwd -6 Senha123) -s /bin/bash
+useradd maria -c "Maria" -m -G GRP_ADM -p $(openssl passwd -6 Senha123) -s /bin/bash
+useradd joao -c "João" -m -G GRP_ADM -p $(openssl passwd -6 Senha123) -s /bin/bash
+
+## Usuarios grupo GRP_VEN
+useradd debora -c "Débora" -m -G GRP_VEN -p $(openssl passwd -6 Senha123) -s /bin/bash
+useradd sebastiana -c "Sebastiana" -m -G GRP_VEN -p $(openssl passwd -6 Senha123) -s /bin/bash
+useradd roberto -c "Roberto" -m -G GRP_VEN -p $(openssl passwd -6 Senha123) -s /bin/bash
+
+## Usuario grupo GRP_SEC
+useradd josefina -c "Josefina" -m -G GRP_SEC -p $(openssl passwd -6 Senha123) -s /bin/bash
+useradd amanda -c "Amanda" -m -G GRP_SEC -p $(openssl passwd -6 Senha123) -s /bin/bash
+useradd rogerio -c "Rogério" -m -G GRP_SEC -p $(openssl passwd -6 Senha123) -s /bin/bash
+
+
+
+# Permissao
+chmod 777 /publico
+chmod 770 /adm
+chmod 770 /ven
+chmod 770 /sec
+
+
+# Grupo diretorios
+chgrp GRP_ADM /adm
+chgrp GRP_VEN /ven
+chgrp GRP_SEC /sec
+
+
+
